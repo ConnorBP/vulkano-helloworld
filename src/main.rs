85608e1909ea431c79a04eebc12849cc5b06d204
Connor Postma <connor.postma@gmail.com>
2019-05-13 13:41:39 -0400

    added a command buffer that copies values between two buffers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::CommandBuffer;
 use vulkano::device::Device;
 use vulkano::device::DeviceExtensions;
 use vulkano::device::Features;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
+use vulkano::sync::GpuFuture;
 
+//winit window creation
+use winit::{EventsLoop, WindowBuilder};
+
+/// Minimal Vulkano implementation based on the docs here: http://vulkano.rs/guide/example-operation
 fn main() {
-    // tries to get an instance to the systems Vulkan installation
+    // tries to get an instance to the systems Vulkan installation TODO: handle vulkan gpu driver or runtime not being installed with an informative popup error message
     let instance =
         Instance::new(None, &InstanceExtensions::none(), None).expect("failed to create instance");
     // returns the first device no matter what (if one exists). First is not necessarily the best.
     let physical = PhysicalDevice::enumerate(&instance)
         .next()
-        .expect("no device available");
+        .expect("no device available"); //TODO: handle the event where none of the devices are compatible with vulkan with an informative popup error message
 
     // enumerates through all queue families on the physical device
     for family in physical.queue_families() {
@@ -61,6 +68,31 @@ fn main() {
     let dest_content = (0..64).map(|_| 0);
     let dest = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), dest_content)
         .expect("failed to create buffer");
+
+    // create a new command buffer to run on our queue family (the one passed in here must be the same one it runs on)
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+        .unwrap()
+        .copy_buffer(source.clone(), dest.clone())
+        .unwrap()
+        .build()
+        .unwrap();
+
+    // submit and sync. Returns an async "GpuFuture" object
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+
+    // using the GpuFuture
+    // signals the fence, makes this thread wait for the gpu result to finish, and then flushes when it finishes
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    // after we are done waiting for the result, we can read the data from each buffer to check if it succeeded
+    let src_content = source.read().unwrap();
+    let dest_content = dest.read().unwrap();
+    assert_eq!(&*src_content, &*dest_content);
 
     println!("Hello, world succeeded!");
 }
